50f4bc8fdd98f1bf55936ac874b46adf0b77d043
Kyle Bradford <kyle.h.bradford@gmail.com>
2023-04-26 18:24:02 -0400

    Revert "Revert "added github username""
    This reverts commit e3467bed488f647e26dd1d3d04cfa5931545466b.

--- Client/main.jsx
+++ Client/main.jsx
@@ -4,11 +4,22 @@ import App from './App'
 import { io } from "socket.io-client"
 import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css'
-const socket = io(import.meta.env.VITE_APP_SERVER);
 
+const socket = io('http://localhost:8080', {
+  path: '/socket.io/',
+  autoConnect: false,
+  withCredentials: true,
+  transportOptions: {
+    polling: {
+      extraHeaders: {
+        "X-NginX-Proxy": "true"
+      }
+    }
+  }
+});
 
-const domain =  import.meta.env.VITE_AUTH0_DOMAIN;
-const clientId =  import.meta.env.VITE_AUTH0_CLIENT_ID;
+// const domain =  import.meta.env.VITE_AUTH0_DOMAIN;
+// const clientId =  import.meta.env.VITE_AUTH0_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App socket={socket}/>
--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,5 @@ RUN npm ci --only=production
 
 # Copy the application code to the container
 COPY . .
-CMD ["node", "Server/index.js"]
+
+CMD ["node", "./Server/index.js"]
